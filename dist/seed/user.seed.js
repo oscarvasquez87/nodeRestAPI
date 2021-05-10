ff2533da8cef1fe35664e20273eed3c898fc8f88
Oscar Vasquez <oscarvasquez87@mail.fresnostate.edu>
2021-05-09 22:51:02 -0700

    push dat one rest api

--- dist/seed/user.seed.js
+++ dist/seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "oscarvasquez87@mail.fresnostate.edu",
+  "username": "ovasquez",
+  "password": "selma2077",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
